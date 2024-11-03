5109b4b8d8eaf7e428964f79995f3bf708add413
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-11-03 11:01:20 +0100

    commented and renamed

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,24 +1,25 @@
 /// <binding BeforeBuild='less' />
-// Less configuration, see https://code.visualstudio.com/Docs/languages/CSS#_transpiling-sass-and-less-into-css
+// Above line allows Visual Studio to trigger the 'less' task before building the project.
+
+// Automate transpilation of .less files, see https://code.visualstudio.com/Docs/languages/CSS#_automating-sassless-compilation
 var gulp = require('gulp');
 var less = require('gulp-less');
 
-gulp.task('less', function (cb) {
+gulp.task('less', function (done) {
     gulp
-        .src('styles.less')
-        .pipe(less())
-        .pipe(
-            gulp.dest(function (f) {
-                return f.base;
-            })
-        );
-    cb();
+        .src('styles.less') // source file(s) to process
+        .pipe(less()) // pass them through the LESS compiler
+        .pipe(gulp.dest(f => f.base)); // Use the base directory of the source file for output
+
+    done(); // signal task completion
 });
 
+// the default task that runs when Gulp is executed without any specific task name
 gulp.task(
     'default',
-    gulp.series('less', function (cb) {
-        gulp.watch('*.less', gulp.series('less'));
-        cb();
+    // Run the 'less' task first, then start watching for changes
+    gulp.series('less', function (done) {
+        gulp.watch('*.less', gulp.series('less'));  // Watch for any changes in .less files and rerun the 'less' task
+        done(); // signal task completion
     })
 );
