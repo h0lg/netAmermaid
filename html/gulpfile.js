78c3c2ed00933da984274027688644cf900303e1
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-11-03 12:12:26 +0100

    added task to generate a HTML diagrammer from template.html and model.json

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -4,6 +4,7 @@
 // Automate transpilation of .less files, see https://code.visualstudio.com/Docs/languages/CSS#_automating-sassless-compilation
 var gulp = require('gulp');
 var less = require('gulp-less');
+var fs = require('fs');
 
 gulp.task('less', function (done) {
     gulp
@@ -12,6 +13,48 @@ gulp.task('less', function (done) {
         .pipe(gulp.dest(f => f.base)); // Use the base directory of the source file for output
 
     done(); // signal task completion
+});
+
+gulp.task('fill-template-html', function (done) {
+    // Read and parse model.json
+    fs.readFile('model.json', 'utf8', function (err, data) {
+        if (err) {
+            console.error('Error reading model.json:', err);
+            done(err);
+            return;
+        }
+
+        const model = JSON.parse(data); // Parse the JSON data
+
+        // Read template.html
+        fs.readFile('template.html', 'utf8', function (err, templateContent) {
+            if (err) {
+                console.error('Error reading template.html:', err);
+                done(err);
+                return;
+            }
+
+            // Replace placeholders in template with values from model
+            let outputContent = templateContent;
+
+            for (const [key, value] of Object.entries(model)) {
+                const placeholder = `{{${key}}}`; // Create the placeholder
+                outputContent = outputContent.replace(new RegExp(placeholder, 'g'), value); // Replace all occurrences
+            }
+
+            // Save the replaced content
+            fs.writeFile('class-diagrammer.html', outputContent, 'utf8', function (err) {
+                if (err) {
+                    console.error('Error writing class-diagrammer.html:', err);
+                    done(err);
+                    return;
+                }
+
+                console.log('class-diagrammer.html generated successfully.');
+                done(); // Signal completion
+            });
+        });
+    });
 });
 
 // the default task that runs when Gulp is executed without any specific task name
